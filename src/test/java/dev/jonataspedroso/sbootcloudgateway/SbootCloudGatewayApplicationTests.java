bce59c1ba9725a924dc86dcb06faf10460344e71
Jonatas Pedroso <jonatasarlindo@hotmail.com>
2023-03-07 20:07:22 -0300

    Configurações e melhorias do gateway

---
+++ src/test/java/dev/jonataspedroso/sbootcloudgateway/SbootCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jonataspedroso.sbootcloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
